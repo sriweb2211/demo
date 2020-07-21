f0a9ed71348aab9cce7900782398bd86f19defbf
Sridhar M <sriweb2211@gmail.com>
2020-07-21 17:40:58 +0530

    Icon color changed to black

--- paar/paar07/js/order.js
+++ paar/paar07/js/order.js
@@ -6,11 +6,17 @@
 
 	$('#qtyText').on('change', function() {
 		var qty = $(this).val();
+
 		if (qty >= 5) {
 			$('#formore').show();
 		} else {
 			$('#formore').hide();
 		}
+
+		// Razorpay adding the quantity
+		// $("div.razorpay-embed-btn").data("url", "https://pages.razorpay.com/pl_FGfvq8ZtC1zoFV/view?amount="+qty);
+		$("div.razorpay-embed-btn").attr("data-url", "https://pages.razorpay.com/pl_FGfvq8ZtC1zoFV/view?amount="+qty);
+		// console.log($("div.razorpay-embed-btn").data("url"));
 	});
 
 })(jQuery)
