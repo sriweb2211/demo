68d1a47cb823ae95b11797253bf798322a79bc95
Sridhar M <sriweb2211@gmail.com>
2020-07-26 11:48:32 +0530

    Script commented

--- paar/paar09/js/order.js
+++ paar/paar09/js/order.js
@@ -14,7 +14,7 @@
 		}
 
 		// Razorpay adding the quantity
-		$("div.razorpay-embed-btn").data("url", "https://pages.razorpay.com/pl_FGfvq8ZtC1zoFV/view?amount="+qty).trigger("refresh");
+		// $("div.razorpay-embed-btn").data("url", "https://pages.razorpay.com/pl_FGfvq8ZtC1zoFV/view?amount="+qty).trigger("refresh");
 		// $("div.razorpay-embed-btn").attr("data-url", "https://pages.razorpay.com/pl_FGfvq8ZtC1zoFV/view?amount="+qty).trigger("refresh");
 		// console.log($("div.razorpay-embed-btn").data("url"));
 
